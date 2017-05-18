d96613410cfe4b761bc6c0b1ee00414c3ad604e0
Mark Bui <tvyybui@yahoo.com>
2017-05-18 01:15:50 -0700

    starting javascript functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var randomStart = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+var wins = 0;
+var losses = 0;
+var userScore = 0;
+var image1 = Math.floor(Math.random() * (12 - 1a + 1)) + 1;
+var image2 = Math.floor(Math.random() * (12 - 1a + 1)) + 1;
+var image3 = Math.floor(Math.random() * (12 - 1a + 1)) + 1;
+var image4 = Math.floor(Math.random() * (12 - 1a + 1)) + 1;
+
+
+
