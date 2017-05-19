92418965ff0949b85d69d76e37d0a4ed6d0381dd
Mark Bui <tvyybui@yahoo.com>
2017-05-18 23:04:28 -0700

    working on functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,53 @@
-var randomStart = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+$(document).ready(function() { 
+var winningNumber = Math.floor(Math.random() * (120 - 19)) + 19;
 var wins = 0;
 var losses = 0;
 var userScore = 0;
-var image1 = Math.floor(Math.random() * (12 - 1a + 1)) + 1;
-var image2 = Math.floor(Math.random() * (12 - 1a + 1)) + 1;
-var image3 = Math.floor(Math.random() * (12 - 1a + 1)) + 1;
-var image4 = Math.floor(Math.random() * (12 - 1a + 1)) + 1;
+var image1 = Math.floor(Math.random() * (12 - 1)) + 1;
+var image2 = Math.floor(Math.random() * (12 - 1)) + 1;
+var image3 = Math.floor(Math.random() * (12 - 1)) + 1;
+var image4 = Math.floor(Math.random() * (12 - 1)) + 1;
 
 
+function addValue1() {
+  userScore += image1;
+  console.log(userScore);
+};
+function addValue2() {
+  userScore += image2;
+  console.log(userScore);
+};
+function addValue3() {
+  userScore += image3;
+  console.log(userScore);
+};
+function addValue4() {
+  userScore += image4;
+  console.log(userScore);
+};
+function showTargetedScore() {
+$('#targetedScore').text(winningNumber);
+}
 
+function score {
+  if (userScore === winningNumber) {
+    wins++;
+    console.log(wins);
+  }
+  else {
+    losses++;
+    console.log(losses);
+  }
+}
+function userScore() {
+  $("#wins").text(wins);
+  $("#losses").text(losses);
+}
+function printScore {
+  $('#score').text('Your total score is: ' + userScore);
+}
+score();
+printScore();
+userScore();
+
+});
