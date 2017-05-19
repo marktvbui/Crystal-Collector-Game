e8b201ddfd0094f5ee881fd0e64748fd0bfad832
Mark Bui <tvyybui@yahoo.com>
2017-05-18 23:14:17 -0700

    working on functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-$(document).ready(function() { 
+// $(document).ready(function() {
 var winningNumber = Math.floor(Math.random() * (120 - 19)) + 19;
 var wins = 0;
 var losses = 0;
@@ -9,45 +9,47 @@ var image3 = Math.floor(Math.random() * (12 - 1)) + 1;
 var image4 = Math.floor(Math.random() * (12 - 1)) + 1;
 
 
-function addValue1() {
-  userScore += image1;
-  console.log(userScore);
-};
-function addValue2() {
-  userScore += image2;
-  console.log(userScore);
-};
-function addValue3() {
-  userScore += image3;
-  console.log(userScore);
-};
-function addValue4() {
-  userScore += image4;
-  console.log(userScore);
-};
-function showTargetedScore() {
-$('#targetedScore').text(winningNumber);
-}
-
-function score {
-  if (userScore === winningNumber) {
-    wins++;
-    console.log(wins);
+  function addValue1() {
+    userScore += image1;
+    console.log(userScore);
   }
-  else {
-    losses++;
-    console.log(losses);
+  function addValue2() {
+    userScore += image2;
+    console.log(userScore);
   }
-}
-function userScore() {
-  $("#wins").text(wins);
-  $("#losses").text(losses);
-}
-function printScore {
-  $('#score').text('Your total score is: ' + userScore);
-}
-score();
-printScore();
-userScore();
+  function addValue3() {
+    userScore += image3;
+    console.log(userScore);
+  }
+  function addValue4() {
+    userScore += image4;
+    console.log(userScore);
+  }
+  function showTargetedScore() {
+  $('#targetedScore').text(winningNumber);
+  console.log(winningNumber);
+  }
 
-});
+  function score() {
+    if (userScore === winningNumber) {
+      wins++;
+      console.log(wins);
+    }
+    else if (userScore > winningNumber) {
+      losses++;
+      console.log(losses);
+    }
+  }
+  function userScore1() {
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+  }
+  function printScore() {
+    $('#score').text('Your total score is: ' + userScore);
+    console.log(userScore);
+  }
+  score();
+  printScore();
+  userScore1();
+
+// });
